961e9b99f0c0d4ecf87bb3b48cd3e2bea1ce4762
shun1kwt4 <kawata.shunichi@mikilab.doshisha.ac.jp>
2024-03-04 18:41:38 +0900

    SidebarCreate

--- app/src/index.js
+++ app/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Sidebar from './component/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+    {/* <App />   //一旦コメントアウト */}
+
+    <Sidebar />
   </React.StrictMode>
 );
 
