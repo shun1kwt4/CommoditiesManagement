3fa8435fe1b427609eaaea78246b15c08fe0da30
akiya125 <akiya2012.2012@icloud.com>
2024-03-11 16:46:52 +0900

    docker compose

--- database/database.sql
+++ database/database.sql
@@ -13,3 +13,14 @@ CREATE TABLE IF NOT EXISTS categories
 (category_id int not null auto_increment, 
 category_name varchar(50) not null,
 PRIMARY KEY (category_name))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS commodities
+(commodity_id int not null auto_increment,
+commodity_name varchar(50) not null,
+borrower_name varchar(100),
+category_id int not null auto_increment, 
+category_name varchar(50) not null,
+fk_category_id(category_id) REFERENCES categories (category_id) ,
+fk_category_name(category_name) REFERENCES categories (category_name),
+fk_borrower_name(borrower_name) REFERENCES students (student_name),
+PRIMARY KEY (commodity_name))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
