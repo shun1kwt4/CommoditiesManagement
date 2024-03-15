5689dffb7e0d15ac67441873d4ac08513d5cfda5
akiya125 <akiya2012.2012@icloud.com>
2024-03-15 17:36:51 +0900

    db

--- database/database.sql
+++ database/database.sql
@@ -1,26 +1,24 @@
-CREATE DATABASE createtb_db;
-USE createtb_db;
+CREATE DATABASE cm_db;
+USE cm_db;
 
 CREATE TABLE IF NOT EXISTS students
-(student_name varchar(100) not null, 
-student_id varchar(100) not null unique,
-email varchar(100) not null, 
-password varchar(128) not null, 
+(id varchar(100) not null unique,
+names varchar(100) not null, 
+emails varchar(100) not null, 
+passwords varchar(128) not null, 
 admin bool not null,
-PRIMARY KEY (student_id))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+PRIMARY KEY(id))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS categories
-(category_id int not null auto_increment, 
-category_name varchar(50) not null,
-PRIMARY KEY (category_name))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+(id int not null auto_increment, 
+names varchar(50) not null,
+PRIMARY KEY(id,names))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS commodities
-(commodity_id int not null auto_increment,
-commodity_name varchar(50) not null,
-borrower_name varchar(100),
-category_id int not null auto_increment, 
-category_name varchar(50) not null,
-fk_category_id(category_id) REFERENCES categories (category_id) ,
-fk_category_name(category_name) REFERENCES categories (category_name),
-fk_borrower_name(borrower_name) REFERENCES students (student_name),
-PRIMARY KEY (commodity_name))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+(id int not null auto_increment,
+names varchar(50) not null,
+states varchar(50) not null DEFAULT '貸出可能',
+categories_id int not null, 
+categories_names varchar(50) not null,
+FOREIGN KEY(categories_id, categories_names) REFERENCES categories(id, names),
+PRIMARY KEY(id))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
