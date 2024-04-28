5a75590935b6f2bee9673608fc8e41f7b3326680
13ellow <mikibun.10969@gmail.com>
2024-04-28 15:13:40 +0900

    modify files

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,13 +1,12 @@
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from "@mui/core/styles";
+import AppBar from "@mui/core/AppBar";
+import Toolbar from "@mui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 // import {makeStyles} from ''
-
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +26,12 @@ export default function Header() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
